3985328b9db663103690cf306e4c7128f30df814
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-02-17 20:13:57 +0530

    Fix syntax and add output for string lengths and even numbers
    Updated the Java code to fix syntax issues and added print statements for output.

--- Basic_Examples/Quick_Practice_Streams.java
+++ Basic_Examples/Quick_Practice_Streams.java
@@ -1,17 +1,21 @@
-public class Main
-{
-	public static void main(String[] args) {
-    
-	  //length of strings
-		List<String> l=Arrays.aslist("Kat","Katy","Katyayani");
-		Stream<String> s=l.stream()
-		                   .map(String::length)
-		                   .collect(Collectors.toList());
-		
-		//Even Numbers
-		List<Integer> li=Arrays.aslist(1,2,3,4,5,6);
-		Stream<String> s1=li.stream()
-		                    .filter(n->n%2==0)
-		                    .collect(Collectors.toList());
-	}
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Length of strings
+        List<String> l = Arrays.asList("Kat", "Katy", "Katyayani");
+        List<Integer> lengths = l.stream()
+                                 .map(String::length)
+                                 .collect(Collectors.toList());
+        System.out.println(lengths); // [3, 4, 10]
+
+        // Even numbers
+        List<Integer> li = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> evenNumbers = li.stream()
+                                      .filter(n -> n % 2 == 0)
+                                      .collect(Collectors.toList());
+        System.out.println(evenNumbers); // [2, 4, 6]
+    }
 }
