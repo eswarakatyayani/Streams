33cd5cd46d7b4432c339d5fd842d8932d12b65d1
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-02-20 19:26:48 +0530

    Refactor stream operations with method references

--- Streams_MethodReference/Stream_MethodReference2.java
+++ Streams_MethodReference/Stream_MethodReference2.java
@@ -5,12 +5,12 @@ public class Stream_MethodReference2 {
 	List<Object> details;
     return details.stream()                                          //Stream<Object>
             .map(o -> mapper.convertValue(o, Book.class))            //Stream<Book>
-            .map(Book::getCollections)                               //Stream<BookCollections>   //Option 3 — Reference to an instance method of an arbitrary object of a specific type
-            .filter(Objects::nonNull)                                                           //Option 1 — Reference to a static method
-            .map(BookCollections::getCustomer)                       //Stream<List<Customer>>   //Option 3 — Reference to an instance method of an arbitrary object of a specific type
+            .map(Book::getCollections)                               //Stream<BookCollections> (Function<T, R>)   //Option 3 — Reference to an instance method of an arbitrary object of a specific type
+            .filter(Objects::nonNull)                                // (Predicate<T>)  (Takes one input, return boolean) //Option 1 — Reference to a static method
+            .map(BookCollections::getCustomer)                       //Stream<List<Customer>> (Function<T, R>)  //Option 3 — Reference to an instance method of an arbitrary object of a specific type
             .filter(list -> list != null && !list.isEmpty())
             .map(list -> list.get(0))                                 //Stream<Customer>
-            .map(Customer::getName)                                   //Stream<String>           //Option 3 — Reference to an instance method of an arbitrary object of a specific type
+            .map(Customer::getName)                                   //Stream<String>  (Function<T, R>) //Option 3 — Reference to an instance method of an arbitrary object of a specific type
             .filter(Objects::nonNull)
             .map(a -> {
                 try {
