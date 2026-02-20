27532ea1abcd6566352df4113f8ecc45e7367745
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2026-02-20 18:26:12 +0530

    Implement orderState method using Stream API

---
+++ Streams_MethodReference/Streams_MethodReference.java
@@ -0,0 +1,34 @@
+public class Stream_MethodReference {
+	public List<Object> orderState(String state) {
+	    List<OrderRecord> orders = getOrdersByState(state);
+	     List<Object> totalDetails = orders.stream()                            //Stream<OrderRecord>   
+	                                          .map(OrderRecord::getOrderId)     //Stream<String>         //option 3: Reference to an instance method of an arbitrary object of a specific type 
+	                                          .map(this::getDetailsFromDB)      //Stream<List<Objects>> //option 2: Reference to an instance method of a particular object
+	                                          .flatMap(List::stream)            //Stream<Objects>       //option 3: Reference to an instance method of an arbitrary object of a specific type
+	                                          .toList();                        //List<Objects>
+	     return totalDetails;
+	     
+	}
+ 
+	private List<OrderRecord> getOrdersByState(String state) {
+		return null;
+	}	
+	
+	 public List<Object> getDetailsFromDB(String searchFieldValue) {
+	        List<Object> listingsResponse=null;
+	        return listingsResponse;
+	    }
+	
+}
+ 
+----------------------------------------------------------------------------
+ 
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OrderRecord {
+    private String orderId;
+    private String orderStatus;
+}
